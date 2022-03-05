0d086c8a75a5ab443e34b7164504840bb5e41631
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2022-03-05 11:09:25 +0100

    Added adjacency matrix generation

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -6,6 +6,14 @@
 5. Algorytm BFS
 
 1. X, Y, Z
+
+  A B C D
+A 0 2 1 3
+B 5 0
+C    0
+D      0
+
+
 """
 import random
 
@@ -17,13 +25,28 @@ def city_generator():
 
 
 def city_distance(x, y):
-    return ((y[0] - x[0]) ** 2 + (y[1] - x[1]) ** 2 + (y[2] - x[2]) ** 2) ** (1/2)
+    return ((y[0] - x[0]) ** 2 + (y[1] - x[1]) ** 2 + (y[2] - x[2]) ** 2) ** (1 / 2)
+
+
+def adjacency_matrix_generator(vertices_list):
+    adjacency_matrix = list()
+    for i in vertices_list:
+        matrix_row = list()
+        for j in vertices_list:
+            if i == j:
+                matrix_row.append(None)
+            else:
+                matrix_row.append(city_distance(i, j))
+        adjacency_matrix.append(matrix_row)
+    return adjacency_matrix
 
 
 if __name__ == '__main__':
-    a = city_generator()
-    b = city_generator()
-    print(a)
-    print(b)
-    print(city_distance(a, b))
+    cities = list()
+    for i in range(0, 5):
+        cities.append(city_generator())
 
+    matrix = adjacency_matrix_generator(cities)
+
+    for x in matrix:
+        print(x)
